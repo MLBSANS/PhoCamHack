001fd5a24dccce86fed53b7d427e41f7648965a4
MLBSANS <nguyendangnhat0987@gmail.com>
2025-02-14 17:12:07 +0700

    Add files via upload

--- open.sh
+++ open.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y python3-venv
+sudo apt update && sudo apt upgrade -y && sudo apt install -y python3 python3-venv python3-pip
 
 if [ ! -d "myenv" ]; then
     python3 -m venv myenv
